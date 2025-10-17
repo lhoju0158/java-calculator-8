6611bf94800259ec7bfe1cfd6cb2bb8429f7b134
lhoju0158 <lhoju0158@gmail.com>
2025-10-17 17:14:32 +0900

    feat(error): 에러메세지 작성

--- src/main/java/calculator/error/ErrorMessage.java
+++ src/main/java/calculator/error/ErrorMessage.java
@@ -1,4 +1,16 @@
 package calculator.error;
 
-public class ErrorMessage {
+public enum ErrorMessage {
+    INVALID_DIVIDERS("유효하지 않은 구문자입니다."),
+    INVALID_NUMBERS("유효하지 않은 수열입니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
