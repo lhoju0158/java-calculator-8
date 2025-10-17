02e320f08b3773f62dcc07cbff4612bc1efd901b
lhoju0158 <lhoju0158@gmail.com>
2025-10-17 19:30:59 +0900

    feat(Application): 문자열 덧셈 계산기 구현 완료

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,10 @@
 package calculator;
 
+import calculator.controller.AddCalculatorController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AddCalculatorController addCalculatorController = new AddCalculatorController();
+        addCalculatorController.start();
     }
 }
