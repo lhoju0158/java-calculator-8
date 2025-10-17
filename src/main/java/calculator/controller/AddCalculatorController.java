ca2941336e28ca0cbf4f92fad6aaadbc0c83720d
lhoju0158 <lhoju0158@gmail.com>
2025-10-17 17:13:40 +0900

    feat(AddCalculatorController): 컨트롤러 코드 작성

--- src/main/java/calculator/controller/AddCalculatorController.java
+++ src/main/java/calculator/controller/AddCalculatorController.java
@@ -1,7 +1,22 @@
 package calculator.controller;
 
+import calculator.service.AddCalculator;
 import calculator.service.Calculator;
+import calculator.view.CalculatorView;
+import calculator.view.CustomCalculatorView;
 
-public class AddCalculator implements Calculator {
+public class AddCalculatorController {
+    private final CalculatorView calculatorView;
+    private final Calculator calculator;
 
+    public AddCalculatorController() {
+        this.calculatorView = new CustomCalculatorView();
+        this.calculator = new AddCalculator();
+    }
+
+    public void start() {
+        String input = calculatorView.InputForm();
+        Integer result = calculator.calculate(input);
+        calculatorView.OutputForm(result);
+    }
 }
